a3a7bace94f4dd257e2504f2cbbe6a6305b89ac8
dosnineco <122121565+dosnineco@users.noreply.github.com>
2023-05-14 18:06:12 -0500

    Update cta.js

--- components/cta.js
+++ components/cta.js
@@ -15,7 +15,7 @@ const Cta = () => {
         </div>
         <div className="flex-shrink-0 w-full text-center lg:w-auto">
           <a
-            href="https://github.com/web3templates"
+            href="https://drive.google.com/file/d/1oWGP55d48OtEwLvjeTUMqkmsRa9RaUgv/view"
             target="_blank"
             rel="noopener"
             className="inline-block py-3 mx-auto text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-10 lg:py-5 ">
