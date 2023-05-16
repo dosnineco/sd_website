8f8b766e071c2386703703f1d2ada725cd57469e
dosnineco <122121565+dosnineco@users.noreply.github.com>
2023-05-16 11:05:58 -0500

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -17,7 +17,7 @@ const Hero = () => {
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               <a
-                href="https://drive.google.com/file/d/1oWGP55d48OtEwLvjeTUMqkmsRa9RaUgv/view"
+                href="https://drive.google.com/file/d/1-FgI8AuEEWy6_BMb4mft2pkfyZa2Wc0T/view?usp=share_link"
                 target="_blank"
                 rel="noopener"
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
